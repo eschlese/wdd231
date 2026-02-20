56e6ea684d15d17e4305d0abd87040be5ce8e36f
salsadebil <188414316+salsadebil@users.noreply.github.com>
2026-02-20 10:25:36 -0700

    fixed weather code

--- final/scripts/weather.js
+++ final/scripts/weather.js
@@ -1,14 +1,14 @@
 const currentTemp = document.querySelector("#current-temp");
-const icon = document.querySelector("weather-icon");
+const icon = document.querySelector("#weather-icon");
 const caption = document.querySelector("figcaption");
-const url = 'https://api.openweathermap.org/data/2.5/weather?lat={49.883747030894135}&lon={-119.45456583590112}&units=metric&appid={232ddc3d81c2d3f3a43c13abfe699830}';
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=49.883747030894135&lon=-119.45456583590112&units=metric&appid=232ddc3d81c2d3f3a43c13abfe699830';
 
 //49.883747030894135, -119.45456583590112
 
 function display(data) {
   currentTemp.innerHTML = `${data.main.temp}&deg;C`;
-  const iconsrc = `https://openweathermap.org/img/w/${data.weather.icon}.png`;
-  caption.textContent = `${data.weather[0].icon}? A perfect day to skate!`;
+  icon.setAttribute('src', `https://openweathermap.org/img/w/${data.weather[0].icon}.png`);
+  caption.textContent = `${data.weather[0].main}? A perfect day to skate!`;
   icon.setAttribute('loading', 'lazy');
 }
 
