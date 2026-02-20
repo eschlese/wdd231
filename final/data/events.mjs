76c2dcd4d9c23ca72712b762b3bb480a50ce306f
salsadebil <188414316+salsadebil@users.noreply.github.com>
2026-02-20 10:52:16 -0700

    added more events to the events page

--- final/data/events.mjs
+++ final/data/events.mjs
@@ -3,7 +3,7 @@ export const events = [
         name: "Knox Mountain Downhill",
         location: "Knox Mountain, Kelowna, BC",
         link: "https://knoxmountaindh.ca/index.html",
-        imgURL: "https://knoxmountaindh.ca/img/h-about.png",
+        imgURL: "https://scontent.fyxd4-1.fna.fbcdn.net/v/t39.30808-6/495350205_10171778976640038_4624790653327041129_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=7e0d18&_nc_ohc=kh0x0dQYT4MQ7kNvwG5Nq7l&_nc_oc=AdkAskPcKJjMutJm6GuaNhj6lu9qOEf0d0LuCof75Bf0hDuwD-qDSsOoOglaiuZxXTX9li0q7304CD1BNF-2WovL&_nc_zt=23&_nc_ht=scontent.fyxd4-1.fna&_nc_gid=jQ_u7p_UBv2xvhvt4NEj2A&oh=00_AfvWWniz3H1F033gT-LGgAmp68INOZgCzpyxpBmlPJe41Q&oe=699E7B41",
         date: "May 31 - June 1, 2025",
     },
     {
@@ -16,30 +16,87 @@ export const events = [
     {
         name: "Learn to Slide Clinic",
         location: "Traders Cove, West Kelowna, BC",
+        link: "https://www.facebook.com/groups/143668038991287/",
         date: "June 19, 2025"
     },
     {
         name: "Kelowna Waterfront Cruise",
         location: "Waterfront Park, Kelowna, BC",
-        link: "https://www.facebook.com/events/1346964799684623/?acontext=%7B%22event_action_history%22%3A[%7B%22surface%22%3A%22home%22%7D%2C%7B%22mechanism%22%3A%22search_results%22%2C%22surface%22%3A%22search%22%7D]%2C%22ref_notif_type%22%3Anull%7D",
-        imgURL: "https://scontent.fyka1-1.fna.fbcdn.net/v/t39.30808-6/494638499_1306400224570206_985088600222719670_n.jpg?stp=dst-jpg_s960x960_tt6&_nc_cat=111&ccb=1-7&_nc_sid=75d36f&_nc_ohc=R0-20MJj_0kQ7kNvwEPXLlq&_nc_oc=Adk9owVGBaW9fi-I2aIHV6mUpCdCcKMFq07AxZVnY18UI61MfIGNNtdRO5X2uESWbaytLCRyAWUt1xr9CG6s5bR1&_nc_zt=23&_nc_ht=scontent.fyka1-1.fna&_nc_gid=VBG2v9Q2KFweqpkiHRcAwQ&oh=00_AfNrsgddjSQc3SMMwrJ8eqWv6AeInJ4G-OhkF4UKAZQMzw&oe=68595EEC",
+        link: "https://www.facebook.com/events/1346964799684623/",
         date: "June 29, 2025"
     },
     {
         name: "Race Practice",
         location: "Pirates Cove, West Kelowna, BC",
+        link: "https://www.facebook.com/groups/143668038991287/",
         date: "June 19, 2025"
     },
     {
         name: "Thursday Night Skate",
         location: "Finch, Lake Country, BC",
+        link: "https://www.facebook.com/groups/143668038991287/",
         date: "June 19, 2025"
     },
     {
         name: "Attack of Danger Bay",
         location: "Pender Harbour, BC",
-        link: "http://www.coastlongboarding.com/",
-        imgURL: "",
+        link: "https://www.coastlongboarding.com/",
+        imgURL: "https://lh4.googleusercontent.com/proxy/xU9rBBIROyrv5Isvqhf7Su2zqSgSGEPBAT7MjYWll1Zfjt7XxT41fG4yyVJU4xvcn7HtZAtABKFAsy-Yi5orVVuHIER-KVdXTyI168Mr3JGMQfnkhBx1jBpnsztL8658lAuOVySnQkI",
         date: "May 14 - May 20, 2026"
+    },
+    {
+        name: "Maryhill Festival of Speed",
+        location: "Maryhill Loops Road, Goldendale, WA",
+        link: "https://www.maryhillratz.com/",
+        imgURL: "https://www.maryhillwindwalk.com/wp-content/uploads/2017/05/18402199_353631391705621_3814214338281863868_o.jpg",
+        date: "June 2025"
+    },
+    {
+        name: "Mount Tabor Downhill Challenge",
+        location: "Mount Tabor Park, Portland, OR",
+        link: "https://www.silverfishlongboarding.com/",
+        imgURL: "https://cdn.shopify.com/s/files/1/0500/3191/9272/files/2023-mt-tabor-poster_1024x1024.jpg?v=1688578446",
+        date: "August 2025"
+    },
+    {
+        name: "L'Escargot Downhill",
+        location: "Peyragudes, France",
+        link: "https://www.escargotdownhill.com/",
+        date: "July 2025"
+    },
+    {
+        name: "Verdicchio Race",
+        location: "Castelplanio, Italy",
+        link: "https://verdicchiorace.it/",
+        imgURL: "https://internationaldownhillfederation.org/media/2017/07/IMG_2363-850x567.jpg",
+        date: "July 2025"
+    },
+    {
+        name: "Teutonia Downhill",
+        location: "Teutonia, Brazil",
+        link: "https://www.instagram.com/teutoniadh/",
+        imgURL: "https://img.redbull.com/images/q_auto,f_auto/redbullcom/2013/11/22/1331621585326_4/topskate-pro-teut%C3%B4nia.jpg",
+        date: "October 2025"
+    },
+    {
+        name: "Sullivan Longboard Challenge",
+        location: "Kimberley, BC",
+        link: "https://www.kimberleyjulyfest.com/",
+        imgURL: "https://static.wixstatic.com/media/a55815_d8b8fce2a9684fdd8f5dac279a4bca3c~mv2.png/v1/fill/w_979,h_552,fp_0.50_0.50,q_90,usm_0.66_1.00_0.01,enc_auto/a55815_d8b8fce2a9684fdd8f5dac279a4bca3c~mv2.png",
+        date: "March 2025"
+    },
+    {
+        name: "Almabtrieb Downhill",
+        location: "Mutters, Austria",
+        link: "https://www.instagram.com/almabtriebdownhill/",
+        imgURL: "https://lh5.googleusercontent.com/proxy/zO-u1MveNQT8RwbxpdJdrTlmiuKCpiLL5z4e45905Zm98_dT26EcgK8eiz68LRv5phY6kN0nOPDWQ8Ru8DTYItE1S1r7aiMepg4RZlPvm8RFEnWqDjbboK1fwQEP0tTqErP2DmO8_Q",
+        date: "September 2025"
+    },
+    {
+        name: "Newton's Nation",
+        location: "Bathurst, NSW, Australia",
+        link: "https://www.newtonsnation.com/",
+        imgURL: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2eme6Ff9gxJBPs-aLu12sWtl7DjiawDUDuA&s",
+        date: "February 2025"
     }
 ]
